3ffea4b47bd180585d9c48c4f6d7dc1c7b6b2979
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-11-03 14:10:36 -0800

    Add files via upload

--- src/mnist.rs
+++ src/mnist.rs
@@ -2,7 +2,7 @@
 
 // returns a basic model with a couple linear and relu layers. by default this outputs a logits distribution, apply .soft_choose to get a model that gives numeric guesses
 pub fn basic_mnist_model()->Graph<Layer<Autodiff<NdArray>>>{
-	let mut graph:Graph<Layer<A>>=Graph::new();
+	let mut graph:Graph<Layer<Autodiff<NdArray>>>=Graph::new();
 
 	graph.connect("input","x").with_clear(true).with(Layer::flatten(-2..=-1));
     graph.connect("x",1_usize).with_clear(true).with(Layer::linear(true,28*28,64,1.0));
@@ -13,8 +13,31 @@ pub fn basic_mnist_model()->Graph<Layer<Autodiff<NdArray>>>{
 
     graph
 }
+/// tests the model, returning the accuracy
+pub fn evaluate_model<B:Backend>(examplecount:usize,graph:&Graph<Layer<B>>)->f32{
+    let graph=Unvec(graph).soft_choose(1.0);
+    let testdata=MNIST::load_validation_data();
+
+	let mut k=0;
+	for n in 0..examplecount{
+		let (testinput,testtarget)=testdata.get(n).unwrap();
+		let expectedoutput=testtarget.into_float_vec()[0] as u32;
+		let testoutput:u32=graph.forward(testinput.reshape([28_isize,28]));
+
+		if expectedoutput==testoutput{k+=1}
+	}
+	k as f32/examplecount as f32*100.0
+}
+/// reads the loss from the .artifact directory
+pub fn read_loss(epoch:usize)->IOResult<f32>{
+	let file=std::fs::read_to_string(&format!(".artifact/epoch-{epoch}/Loss.log"))?;
+	let lines=file.split('\n');
+
+	todo!()
+	//lines.map(|s|s.)
+}
 /// trains the model
-pub fn train_model(graph:Graph<Layer<Autodiff<NdArray>>>)->Graph<Layer<NdArray>>{
+pub fn train_model<B:Backend>(graph:Graph<Layer<Autodiff<B>>>)->Graph<Layer<B>>{
     let batch=32;
 	let epochs=3;
 	let lr=0.001;
@@ -24,7 +47,7 @@ pub fn train_model(graph:Graph<Layer<Autodiff<NdArray>>>)->Graph<Layer<NdArray>>
 	let trainconfig=&trainconfig;
 	let valid=MNIST::load_validation_data();
 
-	let graph=Unvec(graph).wrap_inner().cross_entropy(1.0).set_type::<(Value<A>,Value<A>),LossOutput<A>>().classification().wrap();
+	let graph=Unvec(graph).wrap_inner().cross_entropy(1.0).set_type::<(Value<Autodiff<B>>,Value<Autodiff<B>>),LossOutput<Autodiff<B>>>().classification().wrap();
     let graph=graph.train(trainconfig,optimizer,lr,train,valid);
 	let graph=graph.valid().unwrap_inner();
 
@@ -76,10 +99,10 @@ impl<B:Backend> Dataset < (Value<B>, Value<B>) > for MNIST { // (image, label)
 	}
 }
 struct MNIST{inner:MnistDataset}
-type A=Autodiff<NdArray>;
-use block_graph::{																	// imports from block-graph
+use block_graph::{
 	AI,Graph,Op,Unvec,UnwrapInner,burn::{Layer,LossOutput,Shortcuts,TrainConfig,Value},ops::Reshape
 };
 use burn::{
 	backend::{Autodiff,NdArray},data::dataset::{Dataset,vision::MnistDataset},module::AutodiffModule,optim::AdamWConfig,prelude::Backend
-};															// imports from burn
+};
+use std::{fs::read_to_string,io::Result as IOResult};
